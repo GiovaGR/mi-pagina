1f1e9dd28a11813a18d6a8ac3609e29bc4f4dcc1
GiovaGR <160287442+GiovaGR@users.noreply.github.com>
2026-05-11 20:49:07 -0600

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const boton = document.getElementById("btnSaludo");
-
-if (boton) {
-    boton.addEventListener("click", () => {
-        document.getElementById("mensaje").textContent =
-            "¡Funciona correctamente!";
-    });
-}
